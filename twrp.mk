07ea85ce00f967fb370813bd765387362fb6cb34
Ketut P. Kumajaya <ketut.kumajaya@gmail.com>
2019-01-12 00:18:42 +0000

    wt88047: twrp: Use in recovery vold and vdc
    * Need TWRP patch to build and use in recovery vold and vdc
    
    Change-Id: I0e347ad3ca00e5dc6002216a8fac548f73347238
    Signed-off-by: Ketut P. Kumajaya <ketut.kumajaya@gmail.com>

--- twrp.mk
+++ twrp.mk
@@ -1,5 +1,5 @@
 TARGET_RECOVERY_DEVICE_DIRS += device/wingtech/wt88047/twrp
 TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_USE_SYSTEM_VOLD := true
+TW_CRYPTO_USE_SBIN_VOLD := true
 TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
